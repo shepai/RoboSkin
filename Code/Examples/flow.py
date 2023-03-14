30e58673fe4339e9f0eb927c44f08d60d4b943d8
Dexter Shepherd <dextershepherd@hotmail.co.uk>
2023-03-14 12:06:57 +0000

    reorg

---
+++ Code/Examples/flow.py
@@ -0,0 +1,36 @@
+import cv2
+import letRun #This library can be deleted, it is used for debugging
+import RoboSkin as Skin
+import numpy as np
+
+skin=Skin(videoFile="C:/Users/dexte/github/Chaos-Robotics/movement.avi")
+frame=skin.getFrame()
+old_T=skin.origin
+new=np.zeros_like(frame)
+
+
+while(True):
+    im=skin.getBinary()
+    new=np.zeros_like(frame)
+    t_=skin.getDots(im)
+    t=skin.movement(t_)
+    if t.shape[0]>2:
+        #new[t[:,0],t[:,1]]=(255,0,0)
+        #new[t_[:,0],t_[:,1]]=(0,0,255)
+        #new[old_T[:,0],old_T[:,1]]=(255,0,255)
+        for i,coord in enumerate(t):
+            x1=coord[1]
+            y1=coord[0]
+            x2=old_T[i][1]
+            y2=old_T[i][0]
+            #cv2.putText(new,str(i),(x1,y1),cv2.FONT_HERSHEY_SIMPLEX,0.2,(0,255,0))
+            d=skin.euclid(np.array([x1, y1]), np.array([x2, y2]))
+            if d<25:
+                cv2.arrowedLine(new, (x2, y2), (x1, y1), (0, 255, 0), thickness=1)#"""
+    #show user the imagesS
+    cv2.imshow('spots', new)
+    cv2.imshow('unprocessed', skin.getFrame())
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+skin.close()
+cv2.destroyAllWindows()
---
+++ Code/Examples/letRun.py
@@ -0,0 +1,5 @@
+# some_file.py
+import sys
+# caution: path[0] is reserved for script path (or '' in REPL)
+sys.path.insert(1, 'C:/Users/dexte/github/RoboSkin/Code/')
+
