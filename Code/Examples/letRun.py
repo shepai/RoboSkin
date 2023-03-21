8207d263f575c469015b54cbd6b8928849932a6b
Dexter Shepherd <dextershepherd@hotmail.co.uk>
2023-03-21 17:28:06 +0000

    Update letRun.py
    for linux too

--- Code/Examples/letRun.py
+++ Code/Examples/letRun.py
@@ -2,3 +2,4 @@
 import sys
 # caution: path[0] is reserved for script path (or '' in REPL)
 sys.path.insert(1, 'C:/Users/dexte/github/RoboSkin/Code/')
+sys.path.insert(1, '/its/home/drs25/Documents/GitHub/RoboSkin/Code')
