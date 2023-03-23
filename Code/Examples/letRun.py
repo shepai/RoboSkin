893f26a2f0a122ffeff362feaac62c26b19a5d94
Dexter Shepherd <dextershepherd@hotmail.co.uk>
2023-03-23 10:48:58 +0000

    Update letRun.py

--- Code/Examples/letRun.py
+++ Code/Examples/letRun.py
@@ -1,5 +1,13 @@
 # some_file.py
 import sys
+import os
+path=""
+if os.name == 'nt':
+    path="C:/Users/dexte/github/RoboSkin/Assets/Video demos/"
+else:
+    path="/its/home/drs25/Documents/GitHub/RoboSkin/Assets/Video demos/"
+
 # caution: path[0] is reserved for script path (or '' in REPL)
 sys.path.insert(1, 'C:/Users/dexte/github/RoboSkin/Code/')
 sys.path.insert(1, '/its/home/drs25/Documents/GitHub/RoboSkin/Code')
+
