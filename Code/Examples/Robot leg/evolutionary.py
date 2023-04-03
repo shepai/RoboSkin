74a35a41cdb51874c37a6ece12132fb18d72b052
Dexter Shepherd <dextershepherd@hotmail.co.uk>
2023-04-03 13:59:08 +0100

    new GA

--- Code/Examples/Robot leg/evolutionary.py
+++ Code/Examples/Robot leg/evolutionary.py
@@ -4,6 +4,7 @@ import cv2
 import numpy as np
 from agent import *
 import os
+from copy import deepcopy as DC
 #################################################################
 """
 If the library is not in the direct path add it
@@ -70,19 +71,6 @@ def touchDown(past_Frame,new,SPLIT):
     return tactileN,past_Frame,image
 
 
-old_T=skin.origin
-#create an agent
-arm=Agent(len(old_T.flatten()),[40,40],2)
-genes=np.random.normal(0,3,(arm.num_genes,))
-arm.set_genes(genes)
-
-i=0
-UP=True
-first=False
-LIMIT=50
-dt=0.01
-T=4
-
 def runTrial(agent,T,dt):
     #create sensor and generate initial variables
     frame=skin.getFrame()
@@ -109,6 +97,7 @@ def runTrial(agent,T,dt):
             l.startPos()
             time.sleep(1)
             fit=0
+            break
         else:
             if move==0:
                 l.moveX(1)
@@ -122,17 +111,85 @@ def runTrial(agent,T,dt):
         t+=dt
     return fit/T
 
+def mutate(genotype):
+    return genotype+np.random.normal(0,2,genotype.shape)
 
+def gen_genotype(shape):
+    return np.random.normal(0,3,(shape,))
+
+def run(agent,population,generations=500):
+    pop_size=len(population)
+    shape=population.shape[1]
+    fitness_matrix=np.zeros((pop_size))
+    overTime=np.zeros((generations,))
+
+    for gen in range(generations):
+        clear()
+        print("Generation:",gen,"Fitness",runTrial(arm,2,dt))
+        #get mask to select genotypes for battle
+        mask=np.random.choice([0, 1], size=pop_size)
+        inds=(mask==1).nonzero()[0]
+        while len(inds)%2!=0:
+            mask=np.random.choice([0, 1], size=pop_size)
+            inds=(mask==1).nonzero()[0]
+        #get indicies and tournament modes
+        inds=inds.reshape(len(inds)//2,2).astype(int)
+        fitnesses=np.zeros((len(inds)//2)).astype(int)
+        new_inds=np.zeros((len(inds)//2,2)).astype(int)
+        #run each trial
+        for i in range(len(inds)//2):
+            #select genotypes
+            g1=population[inds[i][0]]
+            g2=population[inds[i][1]]
+            #tournament
+            agent.set_genes(g1)
+            f1=runTrial(agent,T,dt)
+            agent.set_genes(g1)
+            f2=runTrial(agent,T,dt)
+            fitness_matrix[inds[i][0]]=f1
+            fitness_matrix[inds[i][1]]=f2
+            if f1>f2:
+                fitnesses[i]=f1
+                new_inds[i]=[0,i]
+            elif f2>f1:
+                fitnesses[i]=f2
+                new_inds[i]=[1,i]
+            else:
+                fitnesses[i]=0
+                new_inds[i]=[0,i]
+        #get top values and redistribute into the array
+        winners=int(len(inds)//2 *0.4)
+        mutants=int(len(inds)//2 *0.4)
+        other=len(inds)//2 -winners - mutants
+        order=np.argsort(fitnesses)
+        for i in reversed(range(len(inds)//2)): #loop through backwards leaving the winners in place
+            genoWin=new_inds[i][0]
+            old_index=new_inds[i][1]
+            if i<(len(inds)//2)-winners and i>(len(inds)//2)-winners-mutants: #pick mutants
+                population[inds[old_index][1-genoWin]]=mutate(DC(population[inds[old_index][genoWin]])) #mutate copy
+            elif i<(len(inds)//2)-winners-mutants: #the other
+                population[inds[old_index][1-genoWin]]=gen_genotype(shape=shape) #create new genotype
+        overTime[gen]=np.max(fitness_matrix)
+    return overTime
+
+old_T=skin.origin
+#create an agent
+arm=Agent(len(old_T.flatten()),[40,40],2)
+genes=np.random.normal(0,3,(arm.num_genes,))
+arm.set_genes(genes)
+
+i=0
+UP=True
+first=False
+LIMIT=50
+dt=0.01
+T=2
 #genetic agorithm
 SIZE=100
 pop=np.random.normal(0,3,(SIZE,arm.num_genes))
 
-
-
 generations=500
-for gen in range(generations):
-    clear()
-    print("Generation:",gen,"Fitness",runTrial(arm,2,dt))
+run(arm,pop,generations)
     
 skin.close()
 cv2.destroyAllWindows()
