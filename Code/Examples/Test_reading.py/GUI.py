406b97dba96bbc708dd17c96d0fbfc9c2e8c190b
Dexter Shepherd <dextershepherd@hotmail.co.uk>
2023-05-19 16:17:20 +0100

    plot data

--- Code/Examples/Test_reading.py/GUI.py
+++ Code/Examples/Test_reading.py/GUI.py
@@ -8,6 +8,8 @@ import time
 import RoboSkin as sk
 import cv2
 import numpy as np
+import matplotlib.pyplot as plt
+
 B=Board()
 
 #get serial boards and connect to first one
@@ -22,31 +24,134 @@ while COM=="":
     except IndexError:
         time.sleep(1)
 exp=Experiment(B,device=1)
-
-pygame.init()
-(width, height) = (1000, 600)
-screen = pygame.display.set_mode((width, height))
-pygame.display.set_caption('TacTip frame control software')
-running = True
-background_colour = (255,255,255)
-screen.fill(background_colour)
-pygame.display.flip()
-
-#Create slider 1
-slider = Slider(screen, 100, 100, 300, 10, min=0, max=99, step=1)
-output = TextBox(screen, 450, 100, 50, 50, fontSize=30)
-
+screen=None
+slider=None
+sliderZ=None
+output=None
+outputZ=None
+(width1, height2) = (1000, 600)
 slider_width = 20
 slider_height = 300
-slider_x = width // 2 - slider_width // 2
-slider_y = height // 2 - slider_height // 2
-slider_range = height - slider_height
+slider_x = width1 // 2 - slider_width // 2
+slider_y = height2 // 2 - slider_height // 2
+slider_range = height2 - slider_height
+running = True
+background_colour = (255,255,255)
+b1=None
+b1_save=None
+b1_sho=None
+b2=None
+b2_save=None
+b2_sho=None
+b3=None
+b3_save=None
+b3_sho=None
 
-# Create slider 2
-sliderZ = Slider(screen, 100, 200, 300, 10, min=0, max=99, step=1)
-outputZ = TextBox(screen, 450, 200, 50, 50, fontSize=30)
+def init():
+    global screen
+    global slider, output
+    global sliderZ, outputZ
+    global b1, b2, b3, b1_save, b2_save, b3_save, b1_sho, b2_sho, b3_sho
 
-output.disable()  # Act as label instead of textbox
+    pygame.init()
+    
+    screen = pygame.display.set_mode((width1, height2))
+    pygame.display.set_caption('TacTip frame control software')
+    
+    screen.fill(background_colour)
+    pygame.display.flip()
+    #Create slider 1
+    slider = Slider(screen, 100, 100, 300, 10, min=0, max=99, step=1)
+    output = TextBox(screen, 450, 100, 50, 50, fontSize=30)
+
+    # Create slider 2
+    sliderZ = Slider(screen, 100, 200, 300, 10, min=0, max=99, step=1)
+    outputZ = TextBox(screen, 450, 200, 50, 50, fontSize=30)
+
+    output.disable()  # Act as label instead of textbox
+    #create buttons for experiments
+    b1 = Button(
+        screen, 100, 400, 140, 25, text='Speed experiment',
+                fontSize=20, margin=20,
+                inactiveColour=(130, 50, 209),
+                pressedColour=(0, 255, 0), radius=20,
+                onClick=lambda: change_mode("speed")
+                
+            )
+
+    b1_save = Button(
+        screen, 260, 400, 80, 25, text='Save',
+                fontSize=20, margin=20,
+                inactiveColour=(255, 0, 0),
+                pressedColour=(255, 255, 0), radius=20,
+                onClick=lambda: save("speed")
+                
+            )
+
+    b1_sho = Button(
+        screen, 360, 400, 80, 25, text='Plot',
+                fontSize=20, margin=20,
+                inactiveColour=(255, 0, 0),
+                pressedColour=(255, 255, 0), radius=20,
+                onClick=lambda: plot("speed")
+                
+            )
+
+    b2 = Button(
+        screen, 100, 430, 140, 25, text='Pressure experiment',
+                fontSize=20, margin=20,
+                inactiveColour=(130, 50, 209),
+                pressedColour=(0, 255, 0), radius=20,
+                onClick=lambda: change_mode("pressure")
+                
+            )
+
+    b2_save = Button(
+        screen, 260, 430, 80, 25, text='Save',
+                fontSize=20, margin=20,
+                inactiveColour=(255, 0, 0),
+                pressedColour=(255, 255, 0), radius=20,
+                onClick=lambda: save("pressure")
+                
+            )
+
+    b2_sho = Button(
+        screen, 360, 430, 80, 25, text='Plot',
+                fontSize=20, margin=20,
+                inactiveColour=(255, 0, 0),
+                pressedColour=(255, 255, 0), radius=20,
+                onClick=lambda: plot("pressure")
+                
+            )
+
+    b3 = Button(
+        screen, 100, 460, 140, 25, text='Direction experiment',
+                fontSize=20, margin=20,
+                inactiveColour=(130, 50, 209),
+                pressedColour=(0, 255, 0), radius=20,
+                onClick=lambda: change_mode("edges")
+                
+            )
+
+    b3_save = Button(
+        screen, 260, 460, 80, 25, text='Save',
+                fontSize=20, margin=20,
+                inactiveColour=(255, 0, 0),
+                pressedColour=(255, 255, 0), radius=20,
+                onClick=lambda: save("edges")
+                
+            )
+
+    b3_sho = Button(
+        screen, 360, 460, 80, 25, text='Plot',
+                fontSize=20, margin=20,
+                inactiveColour=(255, 0, 0),
+                pressedColour=(255, 255, 0), radius=20,
+                onClick=lambda: plot("edges")
+                
+            )
+
+init()
 
 def change_mode(mo):
     global mode
@@ -105,64 +210,41 @@ def save(id): #save the data if it has beenritten to
         np.save("C:/Users/dexte/github/RoboSkin/code/Models/saved/speed",data_speed)
     if id=="pressure" and np.sum(data_pressure)!=0:
         np.save("C:/Users/dexte/github/RoboSkin/code/Models/saved/pressure",data_pressure)
-    if id=="edge" and np.sum(data_edge)!=0:
+    if id=="edges" and np.sum(data_edge)!=0:
         np.save("C:/Users/dexte/github/RoboSkin/code/Models/saved/edges",data_edge)
-#create buttons for experiments
-b1 = Button(
-    screen, 100, 400, 140, 25, text='Speed experiment',
-            fontSize=20, margin=20,
-            inactiveColour=(255, 0, 0),
-            pressedColour=(0, 255, 0), radius=20,
-            onClick=lambda: change_mode("speed")
-            
-         )
 
-b1_save = Button(
-    screen, 260, 400, 80, 25, text='Save',
-            fontSize=20, margin=20,
-            inactiveColour=(255, 0, 0),
-            pressedColour=(0, 255, 0), radius=20,
-            onClick=lambda: save("speed")
-            
-         )
+def plot(id): #plot the different datas
+    if id=="speed" and np.sum(data_speed)!=0:
+        for i in range(len(data_speed)):
+            plt.plot(speeds,data_speed[i],label="Exp"+str(i))
+        plt.xlabel("Speed setting")
+        plt.ylabel("Average magnitude size (px)")
+        plt.title("How speed affects the time of arrival to the magnitude of vectors")
+        plt.legend(loc="lower left")
+    if id=="pressure" and np.sum(data_pressure)!=0:
+        for i in range(len(data_pressure)):
+            plt.plot([i for i in np.arange(0, CM, ST)],data_pressure[i])
+        plt.xlabel("Pressure in cm")
+        plt.ylabel("Pressure in summed pixels")
+        plt.title("Pressure vs cm")
+    if id=="edges" and np.sum(data_edge)!=0:
+        for i in range(len(data_edge)):
+            f=data_edge[i][0]
+            l=data_edge[i][1]
+            r=data_edge[i][2]
+            weights = np.arange(1, len(f)+1)
+            plt.scatter(f[:,0],f[:,1],c=weights, cmap='Purples')
+            plt.scatter(l[:,0],l[:,1],c=weights, cmap='Greens')
+            plt.scatter(r[:,0],r[:,1],c=weights, cmap='Reds')
+        plt.xlabel("Speed setting")
+        plt.ylabel("Average magnitude size (px)")
+        plt.title("How speed affects the time of arrival to the magnitude of vectors")
+        plt.legend(loc="lower left")
+    plt.show()
+    init()
+    
+init()
 
-
-
-b2 = Button(
-    screen, 100, 430, 140, 25, text='Pressure experiment',
-            fontSize=20, margin=20,
-            inactiveColour=(255, 0, 0),
-            pressedColour=(0, 255, 0), radius=20,
-            onClick=lambda: change_mode("pressure")
-            
-         )
-
-b2_save = Button(
-    screen, 260, 430, 80, 25, text='Save',
-            fontSize=20, margin=20,
-            inactiveColour=(255, 0, 0),
-            pressedColour=(0, 255, 0), radius=20,
-            onClick=lambda: save("pressure")
-            
-         )
-
-b3 = Button(
-    screen, 100, 460, 140, 25, text='Direction experiment',
-            fontSize=20, margin=20,
-            inactiveColour=(255, 0, 0),
-            pressedColour=(0, 255, 0), radius=20,
-            onClick=lambda: change_mode("edges")
-            
-         )
-
-b3_save = Button(
-    screen, 260, 460, 80, 25, text='Save',
-            fontSize=20, margin=20,
-            inactiveColour=(255, 0, 0),
-            pressedColour=(0, 255, 0), radius=20,
-            onClick=lambda: save("edges")
-            
-         )
 currentVal=50
 currentValZ=50
 
@@ -196,9 +278,15 @@ data_pressure=np.zeros((samples,len(np.arange(0, CM, ST))))
 
 while running:
     #add colouring if data exists
-    if np.sum(data_speed)!=0: b1_save.inactiveColour=(0,255,0)
-    if np.sum(data_pressure)!=0: b2_save.inactiveColour=(0,255,0)
-    if np.sum(data_edge)!=0: b3_save.inactiveColour=(0,255,0)
+    if np.sum(data_speed)!=0: 
+        b1_save.inactiveColour=(0,255,0)
+        b1_sho.inactiveColour=(0,255,0)
+    if np.sum(data_pressure)!=0: 
+        b2_save.inactiveColour=(0,255,0)
+        b2_sho.inactiveColour=(0,255,0)
+    if np.sum(data_edge)!=0: 
+        b3_save.inactiveColour=(0,255,0)
+        b3_sho.inactiveColour=(0,255,0)
 
     events=pygame.event.get()
     for event in events:
@@ -228,6 +316,7 @@ while running:
     if mode=="pressure":
         #pressure experiment
         if outer_j==0: #first trial
+            sho_()
             Image=exp.skin.getBinary() #get initial image
             exp.move_till_touch(Image) #be touching the platform
             sho_()
@@ -257,6 +346,7 @@ while running:
     elif mode=="speed":
         #speed experiment
         if outer_j==0:
+            sho_()
             exp.skin.reset()
             #self.moveX(1,-1)
             old_T=exp.skin.origin
@@ -293,6 +383,7 @@ while running:
     elif mode=="edges":
         #edges experiment
         if outer_j==0:
+            sho_()
             exp.skin.reset()
             old_T=exp.skin.origin
             #self.move_till_touch(Image) #be touching the platform
@@ -345,4 +436,5 @@ while running:
     if mode=="menu": 
         pygame_widgets.update(events)
     pygame.display.update()
+    pygame.display.set_mode((width1, height2))
 
