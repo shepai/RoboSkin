ff4fa55342f60e8f5c59f0fa8b6ec1dae9eb1e67
Dexter Shepherd <dextershepherd@hotmail.co.uk>
2023-06-02 12:09:51 +0100

    change in name

--- Code/Examples/Test_reading/speed_exp.py
+++ Code/Examples/Test_reading/speed_exp.py
@@ -17,32 +17,30 @@ while COM=="":
 c=0
 ex=Experiment(B)
 ex.moveZ(1,1)
+ex.moveX(2.5,1)
 samples=10
-samples_2=3
-data=np.zeros((samples,3,samples_2,2))
-
+speeds=[10,20,30]
+data=np.zeros((samples,len(speeds)))
 for i in range(samples):
     print("Trial:",i+1)
-    f,l,r=ex.run_edge(samples_2,True,True,True) #get directions
+    f=ex.test_speed(speeds)
     ex.moveZ(2,1)
     #save to main data
-    data[i][0]=f
-    data[i][1]=l
-    data[i][2]=r
-    weights = np.arange(1, samples_2+1)
-    plt.scatter(f[:,0],f[:,1],c=weights, cmap='Purples')
-    plt.scatter(l[:,0],l[:,1],c=weights, cmap='Greens')
-    plt.scatter(r[:,0],r[:,1],c=weights, cmap='Reds')
-    np.save("C:/Users/dexte/github/RoboSkin/code/Models/saved/dirs",data)
+    data[i]=f
+    weights = np.arange(1, len(speeds)+1)
+    plt.plot(speeds,f,label="Exp"+str(i))
+    np.save("C:/Users/dexte/github/RoboSkin/code/Models/saved/speeds",data)
 
 #plot the labels
 """plt.scatter(f[:,0],f[:,1],c=weights, cmap='Purples',label="Central")
 plt.scatter(l[:,0],l[:,1],c=weights, cmap='Greens',label="Top")
 plt.scatter(r[:,0],r[:,1],c=weights, cmap='Reds',label="Bottom")
 plt.legend(loc="upper right")"""
-plt.colorbar()
-plt.title("Scatter of average vector of different sensations for vectors")
+plt.xlabel("Speed setting")
+plt.ylabel("Average magnitude size (px)")
+plt.title("How speed affects the time of arrival to the magnitude of vectors")
+plt.legend(loc="lower left")
 plt.show()
 
-np.save("C:/Users/dexte/github/RoboSkin/code/Models/saved/dirs",data)
+
 
