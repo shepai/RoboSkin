02ba52a2e71c74a3be5cd3dc426dad6d49d2116e
Dexter Shepherd <dextershepherd@hotmail.co.uk>
2023-06-19 11:57:15 +0100

    Create record.py

---
+++ Code/Examples/record.py
@@ -0,0 +1,22 @@
+import letRun #This library can be deleted, it is used for debugging
+import RoboSkin as sk
+import numpy as np
+import cv2
+
+path= letRun.path
+skin=sk.Skin(device=1)#videoFile=path+"Movement4.avi") #load skin object using demo video
+
+p=np.concatenate(skin.getFrame(),axis=1)
+h1, w1 = p.shape[:2]
+out = cv2.VideoWriter('CloseUp1.avi',cv2.VideoWriter_fourcc(*'DIVX'), 15, (w1,h1))
+
+while True:
+    frame=skin.getFrame()
+    cv2.imshow('frame', frame)
+    
+    q=cv2.waitKey(1) 
+    if q & 0xFF == ord('q'):
+        break
+    out.write(frame) #uncomment to record video
+
+out.release()
